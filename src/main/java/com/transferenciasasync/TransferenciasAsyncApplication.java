052cd1d370f39884712fa6c55c23c277bdd9c56b
Nicolas <nicogarcia1229@gmail.com>
2024-07-22 12:38:43 -0300

    prim

---
+++ src/main/java/com/transferenciasasync/TransferenciasAsyncApplication.java
@@ -0,0 +1,13 @@
+package com.transferenciasasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferenciasAsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransferenciasAsyncApplication.class, args);
+    }
+
+}
